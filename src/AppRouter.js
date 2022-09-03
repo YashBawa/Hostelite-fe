99f0e898b9409ceaf09d9e0b47213b7a0536f449
Pranshu Aggarwal <70687348+Pranshu1902@users.noreply.github.com>
2022-09-03 23:30:02 +0530

    debugged

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,11 @@
 import { useRoutes } from "raviger";
-import Dashboard from "./Common/Dashboard";
+import Loginpg from "./Components/Loginpg";
 import Home from "./Components/Home";
 import HomePage from "./Components/HomePage";
 
 const route = {
   "/": () => <Home />,
-  "/login": () => <Loginpg/>
+  "/login": () => <Loginpg />,
   "/home": () => <HomePage />,
 };
 
