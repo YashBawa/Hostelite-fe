132eb67e3d3f706a575a79b50bccbe0ae0204a5e
Pranshu1902 <pranshu1902@gmail.com>
2022-09-03 23:24:40 +0530

    completed dashboard

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,12 @@
 import { useRoutes } from "raviger";
+import Dashboard from "./Common/Dashboard";
 import Home from "./Components/Home";
+import HomePage from "./Components/HomePage";
 
 const route = {
   "/": () => <Home />,
+  "/dash": () => <Dashboard />,
+  "/home": () => <HomePage />,
 };
 
 export default function AppRouter() {
