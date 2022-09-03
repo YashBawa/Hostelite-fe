0eb74abd5112b6f9d1c8cfd5cfa9335d5b6bd506
Pranshu1902 <pranshu1902@gmail.com>
2022-09-03 13:51:59 +0530

    added basic home pafe

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,19 @@
-import Header from "../Common/Header";
+import logo from "../Images/logo.png";
+// import Header from "../Common/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gradient-to-b from-[#EDEAD0]  to-[#A0E8AF] min-h-screen p-2 text-[#86BAA1]">
       {/* <Header /> */}
       <div>
-        <div className="flex justify-center items-center w-full">
+        <div className="flex text-6xl font-extrabold justify-center items-center w-full text-[#3AB795]">
+          {/* <img width={50} src={logo} alt="" /> */}
           <p>Hostelite</p>
+        </div>
+        <div className="pt-6">
+          <p className="text-3xl flex justify-center ">
+            One place to manage all the paperwork you have in a hostel{" "}
+          </p>
         </div>
       </div>
     </div>
